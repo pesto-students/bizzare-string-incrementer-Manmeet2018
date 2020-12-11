f17cdf5d8d8dcf54b0efa136fa36bb412fab0e55
Manmeet2018 <manietdavv@gmail.com>
2020-12-11 22:00:27 +0530

    feat: Added exception for empty input and added JS DOC comments

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,23 +1,39 @@
-// Start your implementation here
+/**
+ * Create a new secondSmallestAndLargest
+ * @function bizarreStringIncrementer.
+ * @param {String} - String to which 1 is to be added. 
+ * @return {String} - resultant String to which need 1 sould be added.
+ */
+
 const bizarreStringIncrementer = (string) => {
+    if (typeof string != "string")
+        throw "Please enter String."
+
     const temp = string.split('');
     const size = temp.length;
     let idx = size - 1;
     let carry = 1;
     let bizarreString = "";
-    while(idx >= 0 && temp[idx]-'0' >= 0 && temp[idx]-'0' <= 9) {
-        const number = temp[idx] -'0' + carry;
-        carry = (number >= 10) ? ((number)/10) : 0;
-        bizarreString +=  ((number)%10).toString();
+
+    while (idx >= 0 && temp[idx] - '0' >= 0 && temp[idx] - '0' <= 9) {
+        const number = temp[idx] - '0' + carry;
+        carry = (number >= 10) ? ((number) / 10) : 0;
+        bizarreString += ((number) % 10).toString();
         idx--;
     }
-    if(carry)
+
+    if (carry)
         bizarreString += "1";
+
     bizarreString = bizarreString.split('').reverse().join('');
-    if(idx == 0) return bizarreString;
-    return string.substr(0, idx+1) + bizarreString;
+
+    if (idx == 0) return bizarreString;
+
+    return string.substr(0, idx + 1) + bizarreString;
 }
+
 /*
+Test Case
 console.log(bizarreStringIncrementer("foo"));
 console.log(bizarreStringIncrementer("foo23"));
 console.log(bizarreStringIncrementer("foo0041"));
@@ -27,6 +43,12 @@ console.log(bizarreStringIncrementer("f99oo"));
 console.log(bizarreStringIncrementer("f99oo23"));
 console.log(bizarreStringIncrementer("f99oo099"));
 console.log(bizarreStringIncrementer("f99oo0099"));
+console.log(bizarreStringIncrementer("f99oo9999"));
+
+try {
+    bizarreStringIncrementer();
+}
+catch (err) {
+    console.log(err);
+}
 */
-
-
